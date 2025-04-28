36523b9f90b38637ac22525bbc0dde914da613ed
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2025-04-28 12:02:40 +0000

    pytest dev in containaer

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,26 +3,33 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import os
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 SELENIUM_URL = os.getenv('SELENIUM_URL', 'http://localhost:4444/wd/hub')
 
 TARGET_URL = os.getenv('TARGET_URL', 'http://example.com')
-
+print(TARGET_URL)
 
 def test_insert_pages_through_admin():
 
-    # Connect to remote Chrome browser
-    driver = webdriver.Remote(
-        command_executor=SELENIUM_URL,
-        options=chrome_options
-    )
+    try:
+        # Connect to remote Chrome browser
+        driver = webdriver.Remote(
+            command_executor=SELENIUM_URL,
+            options=chrome_options
+        )
 
-    driver.get(TARGET_URL)
-    assert "Example Domain" in driver.title
+        driver.get(TARGET_URL)
+        driver.implicitly_wait(10)
+        assert "Example Domain" in driver.title
+    finally:
+        driver.quit()    
+        
+
 
     print("Test passed!")
+    
+    
 
-    driver.quit()
